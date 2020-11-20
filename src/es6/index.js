283cfbc450f994515c18936ebb41d3f6b38d92d9
Sedarsktian <juan99sebastianmelo@hotmail.com>
2020-11-20 13:33:23 -1000

    let y const

--- src/es6/index.js
+++ src/es6/index.js
@@ -20,3 +20,41 @@ let epicPhrase = hello + '' + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} $ {}`;
 console.log(epicPhrase2);
+
+let lorem = "Vivir es morir pero la muerte puede esperar \n" 
++ "los dulces no tienen sentido comun por eso son dulces."
+
+let lorem2 = `los dulces no tienen sentido comun por esos son dulces.`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    'name': 'sebas',
+    'age': 18,
+    country: 'CO'
+    }
+
+    console.log(person,name, person.age);
+
+    let { name } = person;
+    console.log(name);
+
+    let team1 = ['sebas', 'robotl', 'robot2' ];
+    let team2 = ['sebas1', 'robot3', 'robot4' ];
+
+    let education = ['David', ...team1, ...team2];
+
+    console.log(education);
+    
+    {
+        var globalVar = "Global Var";
+    }
+
+    {
+        let globalLet = 'Global Let';
+    }
+
+
+
+
